e7aad814ea31b9200e002cc0c42f24848384a597
carryme9527 <carryme9527@users.noreply.github.com>
2017-05-11 00:21:18 +0800

    using np for accuracy calculation

--- SuperSimpleML/evaluation.py
+++ SuperSimpleML/evaluation.py
@@ -1,10 +1,7 @@
 # -*- coding: UTF-8 -*-
 
+import numpy as np
+
 
 def calculateAccuracy(y, y_):
-    correct = 0
-    for i in range(len(y)):
-        if y_[i] == y[i]:
-            correct += 1
-    accuracy = 100 * float(correct) / len(y)
-    return accuracy
+    return np.average(y == y_)
