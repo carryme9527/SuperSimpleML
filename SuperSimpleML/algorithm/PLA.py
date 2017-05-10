71b60f70d87fa234c6d2956bc8b694c40b27865c
carryme9527 <carryme9527@users.noreply.github.com>
2017-05-11 02:05:49 +0800

    refactor

--- SuperSimpleML/algorithm/PLA.py
+++ SuperSimpleML/algorithm/PLA.py
@@ -4,23 +4,19 @@ import numpy as np
 
 
 class PLA():
-    def __init__(self):
-        pass
 
     def test(self, X):
-        X_ = np.append(X, np.ones((X.shape[0], 1)), axis=1)
+        X_ = np.append(X, np.ones((len(X), 1)), axis=1)
         y_ = np.sign(np.inner(self.W, X_))
         return y_
 
     def train(self, X, y):
-        dim = X.shape[1]
-        self.W = np.zeros(dim + 1)
+        X_ = np.append(X, np.ones((len(X), 1)), axis=1)
+        self.W = np.zeros(X_.shape[1])
         while True:
-            for i in range(X.shape[0]):
-                x = np.append(X[i], [1])
-                y_ = np.sign(np.inner(self.W, x))
-                if y_ != y[i]:
-                    self.W = self.W + y[i] * x
+            for x_, y_ in zip(X_, y):
+                if np.sign(np.inner(self.W, x_)) != y_:
+                    self.W = self.W + y_ * x_
                     break
             else:
                 return
--- SuperSimpleML/algorithm/PocketPLA.py
+++ SuperSimpleML/algorithm/PocketPLA.py
@@ -1,33 +1,24 @@
 # -*- coding: UTF-8 -*-
-
-from PLA import PLA
 import numpy as np
-from ..evaluation import calculateAccuracy
-
 
 class PocketPLA():
-    def __init__(self):
-        pass
-
     def test(self, X):
-        X_ = np.append(X, np.ones((X.shape[0], 1)), axis=1)
+        X_ = np.append(X, np.ones((len(X), 1)), axis=1)
         y_ = np.sign(np.inner(self.W, X_))
         return y_
 
     def train(self, X, y, T=5):
-        dim = X.shape[1]
-        best_W = np.zeros(dim + 1)
+        X_ = np.append(X, np.ones((len(X), 1)), axis=1)
+        best_W = np.zeros(X_.shape[1])
         best_accuracy = 0
 
         self.W = best_W
         for t in range(T):
-            for i in range(X.shape[0]):
-                x = np.append(X[i], [1])
-                y_ = np.sign(np.inner(self.W, x))
-                if y_ != y[i]:
-                    self.W = self.W + y[i] * x
+            for x_, y_ in zip(X_, y):
+                if np.sign(np.inner(self.W, x_)) != y_:
+                    self.W = self.W + y_ * x_
 
-            accuracy = calculateAccuracy(y, self.test(X))
+            accuracy = np.average(y == self.test(X))
             if accuracy > best_accuracy:
                 best_accuracy, best_W = accuracy, self.W
 
